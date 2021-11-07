a5bf77f366fa8b5475bcf8d06137f8c62ab47847
Drew Peterson <andrew.j.peterson@duke.edu>
2021-11-07 17:45:39 -0500

    progress on controller api

--- src/ooga/controller/UnoController.java
+++ src/ooga/controller/UnoController.java
@@ -1,8 +1,31 @@
 package ooga.controller;
 
+import java.util.function.Consumer;
+
 public class UnoController {
 
-    public UnoController(){
+    /**
+     * initializes data structures and saves the input configuration filepath to a global variable
+     * @param filepath of the configuration file used to define the Uno game parameters
+     */
+    public UnoController(String filepath){
 
     }
+
+    /**
+     * parses the configuration file and initializes the model
+     */
+    public void setupProgram(){
+
+    }
+
+    /**
+     * passes the view's consumer to the model so the model can call .accept() whenever it needs to notify the view of
+     * a change in its state
+     */
+    public void setupConsumer(Consumer viewConsumer){
+
+    }
+
+
 }
