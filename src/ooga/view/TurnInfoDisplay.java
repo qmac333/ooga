0471d7f2b546a7b3d139a892082aaac8c597dd8c
Andrew Sander <ajs149@duke.edu>
2021-11-08 17:28:36 -0500

    Add more APIs for changing the current players and whose turn it is.

---
+++ src/ooga/view/TurnInfoDisplay.java
@@ -0,0 +1,28 @@
+package ooga.view;
+
+import java.util.function.Consumer;
+import ooga.controller.UnoController;
+import ooga.model.PlayersInfo;
+
+public class TurnInfoDisplay implements Consumer<PlayersInfo> {
+
+  /**
+   * Initialize a class that creates the display for the info on the current players and whose turn it is.
+   *
+   * @param controller is a reference to the controller object to pass the consumer through to the
+   *                   model
+   */
+  public TurnInfoDisplay(UnoController controller) {
+
+  }
+
+  /**
+   * Updates the player display based on a change in the model.
+   *
+   * @param playersInfo contains information about the players in the game, whose turn it is, and the direction of play.
+   */
+  @Override
+  public void accept(PlayersInfo playersInfo) {
+
+  }
+}
