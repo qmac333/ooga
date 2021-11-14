a969c886a87319884d09249dc42a847100c2cfd1
Quentin MacFarlane <qtm@duke.edu>
2021-11-14 18:29:30 -0500

    initial version of hand list display

--- src/ooga/view/HandListDisplay.java
+++ src/ooga/view/HandListDisplay.java
@@ -1,11 +1,22 @@
 package ooga.view;
 
+import java.io.FileInputStream;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
+
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import ooga.controller.UnoController;
 
-public class HandListDisplay implements Consumer<Map<Integer, List<List<String>>>> {
+public class HandListDisplay implements Consumer<List<List<String>>> {
+
+  private List<List<String>> currentCards;
 
   /**
    * Initialize a class that creates the display for an UNO hand.
@@ -17,14 +28,33 @@ public class HandListDisplay implements Consumer<Map<Integer, List<List<String>>
 
   }
 
+  public Node createHand() {
+    HBox handList = new HBox();
+    handList.setSpacing(20);
+    StackPane stack = new StackPane();
+    for (List<String> cardProps : currentCards) {
+      try {
+        Rectangle card = new Rectangle(30, 25);
+        Image image = new Image(new FileInputStream(String.format("./data/%s", cardProps.get(0))));
+        ImageView imageView = new ImageView(image);
+        stack.getChildren().addAll(card, imageView);
+        handList.getChildren().add(stack);
+      } catch(Exception e) {
+
+      }
+
+    }
+    return handList;
+  }
+
   /**
    * Takes an update to the player's hand, and redraws the hand
    *
-   * @param integerListMap is a map with keys as card numbers, and values as groups of (color,
+   * @param currentCards is a map with keys as card numbers, and values as groups of (color,
    *                       label) for each of the cards of that number
    */
   @Override
-  public void accept(Map<Integer, List<List<String>>> integerListMap) {
-
+  public void accept(List<List<String>> currentCards) {
+    this.currentCards = currentCards;
   }
 }
