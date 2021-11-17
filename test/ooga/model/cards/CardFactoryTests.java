b6196bfccdfc3cfa4f88a6ebb3465b846a15d2af
will long <wml16@duke.edu>
2021-11-17 17:29:22 -0500

    added cardFactoryTest

---
+++ test/ooga/model/cards/CardFactoryTests.java
@@ -0,0 +1,31 @@
+package ooga.model.cards;
+
+import ooga.model.CardFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import util.DukeApplicationTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CardFactoryTests extends DukeApplicationTest {
+
+    CardFactory factory;
+
+    @BeforeEach
+    public void initializeFactory(){factory = new CardFactory();}
+
+
+    @Test
+    public void initializeNumberCardTest(){
+        try {
+            NumberCard num = (NumberCard) factory.makeCard("Number", 1, "red");
+            assertEquals(1, num.getNum());
+            assertEquals("red", num.getMyColor());
+        }
+        catch(ClassCastException e){
+            System.out.println("card returned not of the right class");
+            Assertions.fail();
+        }
+    }
+}
