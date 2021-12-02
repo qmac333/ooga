c8cbb9a63db715aeb2f9870691b3274e406dbfb6
Andrew Sander <ajs149@duke.edu>
2021-12-02 12:57:34 -0500

    Use update() and render() methods to make tests pass.

--- test/ooga/view/TurnInfoDisplayTest.java
+++ test/ooga/view/TurnInfoDisplayTest.java
@@ -2,6 +2,7 @@ package ooga.view;
 
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import ooga.controller.UnoController;
 import org.junit.jupiter.api.Test;
@@ -19,7 +20,7 @@ public class TurnInfoDisplayTest extends DukeApplicationTest {
     CardDisplay.initializeCards();
     controller = new UnoController(stage);
     controller.createSplashScreen("English");
-    controller.loadFile("data/configurationfiles/validNewFile1.json");
+    controller.loadFile("data/configuration_files/Test Files/validNewFile1.json");
     Button playButton = lookup("#" + SplashScreen.PLAY_CSS_ID).query();
     clickOn(playButton);
 
@@ -27,19 +28,19 @@ public class TurnInfoDisplayTest extends DukeApplicationTest {
 
   @Test
   public void checkInitTable() {
-    // TODO: need to figure out how to call render, even though it is a private method
-//    TurnInfoDisplay display = new TurnInfoDisplay(controller);
-//    // Initial table will be a 3 x 2 table
-//
-//    String[][] expected = {{"Andrew", "7"}, {"Drew", "7"}, {"Quentin", "7"}};
-//
-//
-//    for (int i = 0; i < MockGameViewInterface.NUM_PLAYERS; i++) {
-//      for (int j = 0; j < 2; j++) {
-//        Text text = (Text) display.getTable().getCell(j, i);
-//        assertEquals(expected[i][j], text.getText());
-//      }
-//    }
+    TurnInfoDisplay display = new TurnInfoDisplay(controller);
+    display.update();
+    // Initial table will be a 3 x 2 table
+
+    String[][] expected = {{"Andrew", "7"}, {"Drew", "7"}, {"Quentin", "7"}};
+
+
+    for (int i = 0; i < MockGameViewInterface.NUM_PLAYERS; i++) {
+      for (int j = 0; j < 2; j++) {
+        Text text = (Text) display.getTable().getCell(j, i);
+        assertEquals(expected[i][j], text.getText());
+      }
+    }
   }
 
   @Test
@@ -50,18 +51,10 @@ public class TurnInfoDisplayTest extends DukeApplicationTest {
 
   @Test
   public void reverseDirection() {
-    // TODO: need to figure out how to call render()
-//    controller.getModel().reverseGamePlay();
-//    ImageView arrow = lookup("#" + TurnInfoDisplay.ARROW_CSS).query();
-//    assertEquals(-90, arrow.getRotate()); // arrow is pointing down
-  }
-
-  private int playCard() {
-    return 0;
-  }
-
-  private String sendColor() {
-    return "red";
+    controller.getModel().reverseGamePlay();
+    runAsJFXAction(() -> controller.getUnoDisplay().render());
+    ImageView arrow = lookup("#" + TurnInfoDisplay.ARROW_CSS).query();
+    assertEquals(-90, arrow.getRotate()); // arrow is pointing down
   }
 
 
--- test/ooga/view/UnoDisplayTests.java
+++ test/ooga/view/UnoDisplayTests.java
@@ -15,7 +15,7 @@ public class UnoDisplayTests extends DukeApplicationTest {
     CardDisplay.initializeCards();
     UnoController controller = new UnoController(stage);
     controller.createSplashScreen("English");
-    controller.loadFile("data/configurationfiles/validNewFile1.json");
+    controller.loadFile("data/configuration_files/Test Files/validNewFile1.json");
     Button playButton = lookup("#" + SplashScreen.PLAY_CSS_ID).query();
     clickOn(playButton);
   }
