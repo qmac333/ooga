52ba94bea83e08c09f961e419a5a2aac49b67e36
Drew Peterson <andrew.j.peterson@duke.edu>
2021-12-02 22:44:37 -0500

    Save feature working (no extensive testing)

--- src/ooga/view/UnoDisplay.java
+++ src/ooga/view/UnoDisplay.java
@@ -38,6 +38,7 @@ public class UnoDisplay implements GameScreen {
   public UnoDisplay(UnoDisplayController controller, String language) {
     this.controller = controller;
     languageResources = ResourceBundle.getBundle(String.format("ooga.resources.%s", language));
+    controller.getGameState().setSuppliers(() -> playCard(), () -> sendColor());
 
     unoDisplay = new BorderPane();
     myScene = new Scene(unoDisplay, Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT);
